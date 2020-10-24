a96148d7245a14569029c40fb5c2614c26f0d328
Summ Tokhshima <fishfishfish@hash.fyi>
2020-10-24 12:27:56 +0900

    うー☆

--- main.rb
+++ main.rb
@@ -1,7 +1,5 @@
-# [filename, build, run]
-LANGS = {
-
-}
+#!/usr/bin/env ruby
+# coding: UTF-8
 
 require "thread"
 require "securerandom"
@@ -10,6 +8,23 @@ require "webrick"
 require "tmpdir"
 
 def main
+  @lang = JSON.parse(File.read("www/lang.json"))
+
+  @process = {}
+  @wait_queue = Thread::Queue.new
+
+  @pool = 5.times.map do
+    Thread.fork do
+      loop do
+        id = @wait_queue.deq
+        data = @process[id]
+        run_program(data)
+        sleep 5
+        @process.delete id
+      end
+    end
+  end
+
   server = WEBrick::HTTPServer.new({
     DocumentRoot: "./www/",
     BindAdress: "127.0.0.1",
@@ -25,13 +40,12 @@ def main
   server.start
 end
 
-ProcessData = Struct.new(:request, :status, :result)
+ProcessData = Struct.new(:id, :request, :status, :result)
+RequestData = Struct.new(:LanguageId, :sourceCode, :input)
 
 def init_server(server)
-  @processes = {}
-  @wait_queue = Thread::Queue.new
 
-  # POST {LanguageId, sourceCode, input} -> {status, id}
+  # POST {LanguageId, sourceCode, input} -> {status, result}
   server.mount_proc("/submit") do |req, res|
     res.content_type = "application/json"
 
@@ -40,24 +54,27 @@ def init_server(server)
       next
     end
 
-    data = JSON.parse(req.body)
-    if (reason = validate_request(data))
+    begin
+      data = parse_request(JSON.parse(req.body))
+    rescue => reason
       res.body = error_json(reason)
       next
     end
 
     id = generate_id
 
-    @processes[id] = ProcessData[data, :waiting, nil]
+    puts "accepted #{id}"
+
+    @process[id] = ProcessData[id, data, :waiting, nil]
     @wait_queue.enq id
 
     res.body = JSON.generate({
       status: "success",
-      id: id,
+      result: id,
     })
   end
 
-  # GET {id} -> {status, id, process: {status, result?: {exitcode, stdout, stderr, memory, time}}}
+  # GET {id} -> {status, result: {status, result?: {exitcode, stdout, stderr, memory, time}}}
   server.mount_proc("/status") do |req, res|
     res.content_type = "application/json"
 
@@ -77,34 +94,29 @@ def init_server(server)
 
     res.body = JSON.generate({
       status: "success",
-      id: id,
-      process: {
+      result: {
         status: process[:status],
         result: process[:result],
       },
     })
   end
 
-  @pool = 5.times.map do
-    Thread.fork do
-      loop do
-        id = @wait_queue.deq
-        data = @processes[id]
-        run_program(data)
-      end
-    end
-  end
+  server.mount("/", WEBrick::HTTPServlet::FileHandler, "./www")
 end
 
 def run_program(data)
   data.status = :running
+  id = data.id
 
-  tmpdir do
-    lang = LANGS[data.request["lang"]]
+  Dir.mktmpdir do |dir|
+    lang = @lang[data.request.LanguageId]
+    files = lang["files"].map { |filename| File.join(dir, filename) }
+    compile_error = File.join(dir, "compile_error.txt")
+    runtime_error = File.join(dir, "runtime_error.txt")
+    time_file = File.join(dir, "time.txt")
 
-    source_file = lang[0]
-    File.open(source_file, "w") { |f| f << data.request["sourceCode"] }
-    data.request["sourceCode"] = nil
+    File.open(files[0], "w") { |f| f << data.request.sourceCode }
+    data.request.sourceCode = nil
 
     result = {
       exitcode: nil,
@@ -116,56 +128,71 @@ def run_program(data)
     data.result = result
 
     # compile
-    IO.popen(lang[1], "r+", err: "compile_error.txt") do |io|
-      stdput = io.read
-      io.close
-      exitcode = $?.exitcode
-      unless exitcode == 0
-        result.exitcode = exitcode
-        data.status = :compile_error
+    if lang["compile"]
+      compile_command = lang["compile"].gsub(/\$\d+/) { |m| files[m[1..-1].to_i] }
+      puts "compile #{id}"
+      IO.popen(compile_command, "r+", err: compile_error) do |io|
+        stdput = io.read
+        io.close
+        unless $?.exitstatus == 0
+          result[:exitcode] = $?.exitstatus
+          result[:stderr] = File.read(compile_error)
+          data.status = :compile_error
+        end
       end
     end
 
-    next if data.status == :compile_error
+    if data.status == :compile_error
+      puts "compile_error #{id}"
+      next
+    end
 
     # run
+    run_command = lang["run"].gsub(/\$\d+/) { |m| files[m[1..-1].to_i] }
+    puts "run #{id}: $ #{run_command}"
+    IO.popen("timeout -s KILL 10 time -f '%e %M' -o '#{time_file}' #{run_command}", "r+", err: runtime_error) do |io|
+      io << data.request.input
+      result[:stdout] = io.read
+      io.close
+      result[:exitcode] = $?.exitstatus
+      data.status = :compile_error unless $?.exitstatus == 0
+    end
 
+    result[:stderr] = File.read(runtime_error)
+    time, memory = File.read(time_file).split.map &:to_f
+    result[:time] = time * 1e3
+    result[:memory] = memory
+
+    puts "finish #{id}"
+
+    data.status = :finished unless data.status == :runtime_error
   end
 end
 
-def validate_request(data)
-  unless data.key? "lang"
-    return "lang not specified"
-  end
-  unless LANGS.key? data["lang"]
-    return "lang not found"
-  end
+def parse_request(data)
+  raise "LanguageId not specified" unless data.key? "LanguageId"
+  raise "LanguageId not found" unless @lang.key? data["LanguageId"]
 
+  raise "sourceCode not specified" unless data.key? "sourceCode"
+
+  raise "input not specified" unless data.key? "input"
+
+  RequestData[data["LanguageId"], data["sourceCode"], data["input"]]
 end
 
 def generate_id
   id = nil
   begin
     id = SecureRandom.uuid
-  end until @processes.key? id
+  end while @process.key? id
   id
 end
 
 def error_json(reason)
   JSON.generate({
     status: "error",
-    message: reason,
+    result: reason,
   })
-end
-
-def tmpdir
-  pwd = Dir.pwd
-  Dir.mktmpdir do |dir|
-    Dir.chdir dir
-    res = yield
-    Dir.chdir pwd
-    res
-  end
 end
 
 main
